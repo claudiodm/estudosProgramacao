9d31e3ea198861125e1102ebc49537504e1e1aa0
claudiodm <iclaudio.machado@gmail.com>
2022-05-04 18:21:51 -0300

    Iniciando no mundo JavaScript

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Para visualizar o retorno deste código você precisa estar com o "Live Server" 
+// instalado no VSCode, clicar com o botão direito do mouse na pagina HTML e selecionar "Open With Live Server". 
+// Assim que o navegador for aberto, que deverá ser o Chrome, você aperta o F12 e seleciona console. 
+// O código estará impresso no console.
+
+iniciandoJS();
+function iniciandoJS() {
+    console.log("iniciando no mundo javaScript!!!!!")    
+}
